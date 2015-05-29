fbc3126e000db3b5ac75b4d07860c118603b2235
Derek Misler <derekmisler@gmail.com>
2015-05-29 17:09:46 -0400

    alright, this one is working for now

--- Python/Web-Scraper/scraper2.py
+++ Python/Web-Scraper/scraper2.py
@@ -1,17 +1,38 @@
-from lxml import html
-import requests
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+from time import sleep # be nice
 
-root_url = 'http://theorangepeel.net'
-index_url = root_url + '/events/blind-maiden/'
+BASE_URL = "http://www.chicagoreader.com"
 
-page = requests.get(index_url)
-tree = html.fromstring(page.text)
+def make_soup(url):
+	html = urlopen(url).read()
+	return BeautifulSoup(html, "lxml")
 
-#This will create a list of event URLs:
-event_title = tree.xpath('h1[@itemprop="headline"]/text()')
-#This will create a list of dates:
-event_date = tree.xpath('span[@class="event-date-sub"]/text()')
+def get_category_links(section_url):
+	soup = make_soup(section_url)
+	boccat = soup.find("dl", "boccat")
+	category_links = [BASE_URL + dd.a["href"] for dd in boccat.findAll("dd")]
+	return category_links
 
-print tree
-print event_title
-print event_date
+def get_category_winner(category_url):
+	soup = make_soup(category_url)
+	category = soup.find("h1", "headline").string
+	winner = [h2.string for h2 in soup.findAll("h2", "boc1")]
+	runners_up = [h2.string for h2 in soup.findAll("h2", "boc2")]
+	return {"category": category,
+			"category_url": category_url,
+			"winner": winner,
+			"runners_up": runners_up}
+
+if __name__ == '__main__':
+	food_n_drink = ("http://www.chicagoreader.com/chicago/best-of-chicago-2011-food-drink/BestOf?oid=4106228")
+
+	categories = get_category_links(food_n_drink)
+
+	data = [] # a list to store our dictionaries
+	for category in categories:
+		winner = get_category_winner(category)
+		data.append(winner)
+		sleep(1) # be nice
+
+	print data
