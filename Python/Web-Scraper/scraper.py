386586af61c518f08e587c3a7bd0ea6c662a3487
Derek Misler <derekmisler@gmail.com>
2015-05-29 15:02:14 -0400

    orangepeel is almost done, fixing a couple of errors

--- Python/Web-Scraper/scraper.py
+++ Python/Web-Scraper/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import bs4
+from multiprocessing import Pool
 
 root_url = 'http://theorangepeel.net'
 index_url = root_url + '/events/'
@@ -7,13 +8,26 @@ index_url = root_url + '/events/'
 def get_event_urls():
 	response = requests.get(index_url)
 	soup = bs4.BeautifulSoup(response.text)
-	return [a.attrs.get('href') for a in soup.select('div.event-details header.article-header h3.h2 a')]
-	
-print(get_event_urls())
+	return [a.attrs.get('href') for a in soup.select('.event-details .article-header .h2 a')]
 
-def get_event_data(event_url):
-	event_data = {}
+def get_orangepeel_data(event_url):
+	orangepeel_data = {}
 	response = requests.get(root_url + event_url)
 	soup = bs4.BeautifulSoup(response.text)
-	event_data['title'] = 
-	
+	try:
+		orangepeel_data['title'] = soup.select('.article-header .entry-title')[0].get_text()
+		##orangepeel_data['subtitle'] = soup.select('.article-header .event-subtitle')[0].get_text()
+		orangepeel_data['date'] = soup.select('.vitals-left .event-date-sub')[0].get_text()
+		orangepeel_data['time'] = soup.select('.vitals-left .event-time')[0].get_text()
+		orangepeel_data['price'] = soup.select('.vitals-left .ticket-price')[0].get_text()
+		##orangepeel_data['tickets_url'] = soup.select('.vitals-right .event-status.onsale a')[0].get_text()
+		##orangepeel_data['facebook_url'] = soup.select('.vitals-right .facebook-rsvp')[0].get_text()
+		##orangepeel_data['description'] = soup.select('item-content')[0].get_text()
+	except:
+		pass
+	return orangepeel_data
+
+def show_orangepeel_data():
+	event_urls = get_event_urls()
+	for event_url in event_urls:
+		print get_orangepeel_data(event_url)
