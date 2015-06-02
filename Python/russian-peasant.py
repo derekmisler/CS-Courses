61c3823ad7dfcddeacfc61d1361a9331af8d2968
derekmisler <derekmisler@gmail.com>
2015-06-02 05:12:28 -0400

    fixed tabs

--- Python/russian-peasant.py
+++ Python/russian-peasant.py
@@ -3,14 +3,14 @@
 ## the Russian Peasant Algorithm
 
 def russia(a,b):
-    x = a
-    y = b
-    z = 0
-    while x > 0:
-        if x % 2 == 1:      ## find every odd number
-            z += y          ## add the partner number to z
-        y = y << 1          ## shift binary is like multiplying by 2
-        x = x >> 1          ## this one is dividing by 2
-    return z
+	x = a
+	y = b
+	z = 0
+	while x > 0:
+		if x % 2 == 1:      ## find every odd number
+			z += y          ## add the partner number to z
+		y = y << 1          ## shift binary is like multiplying by 2
+		x = x >> 1          ## this one is dividing by 2
+	return z
 
-print russia(24,16)
+print russia(32,16)
