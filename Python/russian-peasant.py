4bcc3165c050e8ad80aad69921e32b71451657f9
derekmisler <derekmisler@gmail.com>
2015-06-03 07:02:44 -0400

    added an algorithm test

--- Python/russian-peasant.py
+++ Python/russian-peasant.py
@@ -2,6 +2,10 @@
 ## Output the solution to those two numbers using
 ## the Russian Peasant Algorithm
 
+import time
+
+CACHE = {}
+
 def russia(a,b):
 	x = a
 	y = b
@@ -9,8 +13,15 @@ def russia(a,b):
 	while x > 0:
 		if x % 2 == 1:      ## find every odd number
 			z += y          ## add the partner number to z
-		y = y << 1          ## shift binary is like multiplying by 2
+		y = y << 1          ## shift binary by 1 is like multiplying by 2
 		x = x >> 1          ## this one is dividing by 2
 	return z
 
-print russia(32,16)
+def test_russia():
+	assert russia(357,16) == 5712		## test the algorithm
+
+	start_time = time.time()			## time the algorithm
+	print russia(357,16)
+	print "Russian Algorithm took %f seconds" % (time.time()-start_time)
+
+test_russia()
