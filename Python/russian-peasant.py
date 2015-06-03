7c9cbe26719826fe10be5dc9fef861e13a0922b3
derekmisler <derekmisler@gmail.com>
2015-06-03 07:33:37 -0400

    implemented a caching mechanism

--- Python/russian-peasant.py
+++ Python/russian-peasant.py
@@ -7,21 +7,31 @@ import time
 CACHE = {}
 
 def russia(a,b):
-	x = a
-	y = b
-	z = 0
-	while x > 0:
-		if x % 2 == 1:      ## find every odd number
-			z += y          ## add the partner number to z
-		y = y << 1          ## shift binary by 1 is like multiplying by 2
-		x = x >> 1          ## this one is dividing by 2
+	key = (a,b)
+	if key in CACHE:
+		z = CACHE[key]			## check if key is in the cache
+	else:
+		print 'Not in Cache'
+		x = a
+		y = b
+		z = 0
+		while x > 0:
+			if x % 2 == 1:      ## find every odd number
+				z += y          ## add the partner number to z
+			y = y << 1          ## shift binary by 1 is like multiplying by 2
+			x = x >> 1          ## this one is dividing by 2
+		CACHE[key] = z
 	return z
 
 def test_russia():
-	assert russia(357,16) == 5712		## test the algorithm
-
 	start_time = time.time()			## time the algorithm
 	print russia(357,16)
 	print "Russian Algorithm took %f seconds" % (time.time()-start_time)
+
+	start_time = time.time()			## time the algorithm twice
+	print russia(357,16)				## it should be a bit faster
+	print "Russian Algorithm took %f seconds" % (time.time()-start_time)
+
+	assert russia(357,16) == 5712		## test the algorithm
 
 test_russia()
