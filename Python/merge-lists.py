0bf6b7e6c118b1f1886104f85727d7253802781c
Derek Misler <derekmisler@gmail.com>
2015-06-12 10:22:06 -0400

    added a note about .pop

--- Python/merge-lists.py
+++ Python/merge-lists.py
@@ -4,7 +4,7 @@
 def mergeLists(list1, list2):
 	output = []								# defining an empty list as output
 	while list1 and list2:					# keep looping while both of the lists contain something
-		if (list1[0] < list2[0]):			# check the number at the beginning of both lists, remove the smaller, and append that item onto the output list
+		if (list1[0] < list2[0]):			# check the number at the beginning of both lists, remove the smaller (.pop does this) and append that item onto the output list
 			output.append(list1.pop(0))		# since the two lists are already sorted, just keep moving the smaller item to the output list
 		else:
 			output.append(list2.pop(0))
